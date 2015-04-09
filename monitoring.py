8c2fb22eb7e49edad73fe8d4b7527939d7621c4d
Sergey Romanov <xxsmotur@gmail.com>
2015-04-09 23:55:54 +0500

    Reorganize some part of code

--- monitoring.py
+++ monitoring.py
@@ -1,18 +1,42 @@
 import redis
 import datetime
+from multiprocessing import Process, Pool
+import threading
 
 class Monitoring:
 	def __init__(self, host='localhost', port=6379):
-		self.client = redis.ConnectionPool(host=host, port=port)
+		self.servers = []
+		#self.client = redis.ConnectionPool(host=host, port=port)
 		self.processing = Processing()
+
+	def _createClient(self,host, port):
+		return redis.ConnectionPool(host=host, port=port)
+
+	def addServer(self, host, port):
+		self.servers.append({'host':host, 'port':port})
+
+	def _start_receiving(self, connect):
+		while True:
+			self.processing.receive_response(connect.read_response())
+
+	def _createMonitor(self, server):
+		client = self._createClient(server['host'], server['port'])
+		connect = client.get_connection('monitor', None)
+		connect.send_command('monitor')
+		print("monitoring of server {0}:{1} has started...".format(server['host'], server['port']), datetime.datetime.now())
+		pool = Pool(processes=1)
+		pool.apply_async(self._start_receiving, [connect], callback=self.getter1)
+		pool.close()
+		#pool.join()
+
+	def getter1(self, value):
+		print(value)
+
 
 	def start(self):
 		""" Start monitoring """
-		connect = self.client.get_connection('monitor', None)
-		connect.send_command('monitor')
-		print("monitoring has started...", datetime.datetime.now())
-		while True:
-			self.processing.receive_response(connect.read_response())
+		for server in self.servers:
+			self._createMonitor(server)
 
 class Processing:
 	""" Processing and analytics receive commands """
