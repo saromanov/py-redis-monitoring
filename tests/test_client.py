96df9970a124d9db239d828ad32c79b48f29c4a8
Sergey Romanov <xxsmotur@gmail.com>
2015-11-25 21:46:19 +0500

    test.Client: Update

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,15 +1,27 @@
 import redismonitor
 import redis
+import time
 
 
 class TestClient:
-    cli = redismonitor.ProcessingClient()  # Client initialization
-    rediscli = redis.StrictRedis(host='localhost', port=6380, db=0)
+    cli = redismonitor.ProcessingClient()
+    rediscli = redis.StrictRedis(host='localhost', port=6380)
 
-    def test_first(self):
+    def test_commands_stat1(self):
+        self.cli.removeData()
         self.rediscli.set('foo', 'bar')
-        self.rediscli.set('foo', 'bar2')
+        self.rediscli.set('bar', 'foo')
+        time.sleep(1)
         commands = self.cli.getCommandsStat()
         assert len(commands) == 1
         assert commands[0][0] == 'set'
-        assert commands[0][1] == '2'
+        assert commands[0][1] == 2
+
+    def test_commands_stat(self):
+        self.cli.removeData()
+        self.rediscli.set('foo', 'bar')
+        self.rediscli.lpush('doom', '2')
+        self.rediscli.set('loop', 'noop')
+        time.sleep(1)
+        commands = self.cli.getCommandsStat()
+        assert len(commands) == 2
