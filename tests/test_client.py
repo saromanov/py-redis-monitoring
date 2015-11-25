de1d85250dcd05d8ebffe6168b6f99945c370c1b
Sergey Romanov <xxsmotur@gmail.com>
2015-11-25 20:08:08 +0500

    Add tests

---
+++ tests/test_client.py
@@ -0,0 +1,13 @@
+import redismonitor
+import redis
+
+class TestClient:
+    cli = redismonitor.ProcessingClient() # Client initialization
+    rediscli = redis.StrictRedis(host='localhost', port=6380, db=0)
+    def test_first(self):
+        self.rediscli.set('foo', 'bar')
+        self.rediscli.set('foo', 'bar2')
+        commands = self.cli.getCommandsStat()
+        assert len(commands) == 1
+        assert commands[0][0] == 'set'
+        assert commands[0][1] == '2'
---
+++ tests/test_monitoring.py
@@ -0,0 +1,21 @@
+import redismonitor
+import pytest
+
+@pytest.mark.incremental
+class TestMonitoring:
+    mon = redismonitor.Monitoring()
+    def test_init(self):
+        assert self.mon.host == 'localhost'
+        assert self.mon.port == 6379
+
+    def test_add_server(self):
+        self.mon.addServer('localhost', '6379')
+        self.mon.addServer('localhost', '6380')
+        self.mon.addServer('localhost', '6381')
+        self.mon.addServer('localhost', '6382')
+        assert len(self.mon.servers) == 4
+
+
+    def test_add_notofy(self):
+        self.mon.addNotify('hset', lambda x: x)
+        assert len(self.mon.notifications) == 1
